17a53f1ef2a51da2bb758d88e903b5846e563a20
Andreas Brecl <andreas@brecl.com>
2023-03-15 17:44:43 -0600

    removed orb

--- catkin_ws/build/CTestTestfile.cmake
+++ catkin_ws/build/CTestTestfile.cmake
@@ -5,7 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
-subdirs("realsense-ros/realsense2_description")
 subdirs("pololu_maestro_ros")
-subdirs("realsense-ros/realsense2_camera")
-subdirs("orb_slam_2_ros")
