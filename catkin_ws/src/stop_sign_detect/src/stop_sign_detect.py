c8bf9fb59d607ebf2781a4dcd4f900a7061e7ce9
Ben Spicer <bencspicer@gmail.com>
2023-04-29 18:48:24 -0600

    stop sign detection working

--- catkin_ws/src/stop_sign_detect/src/stop_sign_detect.py
+++ catkin_ws/src/stop_sign_detect/src/stop_sign_detect.py
@@ -6,88 +6,92 @@ import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-class StopSignDetector:
-    def __init__(self):
-        self.bridge = CvBridge()
-        # self.image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.callback)
-        self.image_sub = rospy.Subscriber("stop_sign", Image, self.callback)
+def detect(self, data):
+    debug = False
+    mode = "dog"
+    
+    try:
+        cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+        print(e)
 
-    def callback(self, data):
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            print(e)
+    # mask only red pixels
+    red_lower = np.array([0, 0, 50])
+    red_upper = np.array([150, 150, 255])
+    mask = cv2.inRange(cv_image, red_lower, red_upper)
+    detected_output = cv2.bitwise_and(cv_image, cv_image, mask=mask)
 
-        stop_sign_detected = self.detect_stop_sign(cv_image)
+    # difference of gaussians
+    if mode == "lap":
+        blobs_log = cv2.Laplacian(detected_output, cv2.CV_64F)
+        processed = np.uint8(np.absolute(blobs_log))
+    elif mode == "dog":
+        processed = cv2.GaussianBlur(detected_output, (5,5), 0) - cv2.GaussianBlur(detected_output, (15,15), 0)
+    
+    kernel = np.ones((5, 5), np.uint8)
+    processed = cv2.dilate(processed, kernel, iterations=0)
 
-        if stop_sign_detected:
-            rospy.loginfo("Stop sign detected: True")
-        else:
-            rospy.loginfo("Stop sign detected: False")
+    if debug:
+        cv2.imshow("processed", processed)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
 
-    def detect_stop_sign(self, img):
-        # Load stop sign cascade file
-        # stop_cascade = cv2.CascadeClassifier('path/to/stop_sign_cascade.xml')
+    # Set up the SimpleBlobDetector with default parameters
+    params = cv2.SimpleBlobDetector_Params()
 
-        # # Convert image to grayscale
-        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # params.filterByColor = True
+    # params.blobColor = 0
 
-        # Detect stop signs in the image
-        # stop_signs = stop_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-        red_lower = (50, 0, 0)
-        red_upper = (255, 150, 150)
-        mask = cv2.inRange(img, red_lower, red_upper)
-        cv2.imshow("Win", mask)
-        # mask = cv2.erode(mask, None, iterations=0)
-        # mask = cv2.dilate(mask, None, iterations=0)
-        # frame = cv2.bitwise_and(frame,frame,mask = mask)
-        # Set up the SimpleBlobDetector with default parameters
-        params = cv2.SimpleBlobDetector_Params()
+    # Set the threshold
+    params.minThreshold = 0
+    params.maxThreshold = 255
 
-        # Set the threshold
-        params.minThreshold = 10
-        params.maxThreshold = 200
+    # # Set the area filter
+    params.filterByArea = True
+    params.minArea = 3000
+    params.maxArea = 1000000
 
-        # # Set the area filter
-        params.filterByArea = True
-        params.minArea = 100
-        params.maxArea = 1000
+    # # Set the circularity filter
+    params.filterByCircularity = True
+    params.minCircularity = .3
+    params.maxCircularity = 1
 
-        # # Set the circularity filter
-        params.filterByCircularity = True
-        params.minCircularity = 0.1
-        params.maxCircularity = 1
+    # # Set the convexity filter
+    params.filterByConvexity = True
+    params.minConvexity = 0.2
+    params.maxConvexity = 1
 
-        # # Set the convexity filter
-        params.filterByConvexity = True
-        params.minConvexity = 0.87
-        params.maxConvexity = 1
+    # # Set the inertia filter
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.1
+    params.maxInertiaRatio = 1
 
-        # # Set the inertia filter
-        # params.filterByInertia = True
-        # params.minInertiaRatio = 0.01
-        # params.maxInertiaRatio = 1
-
-        detector = cv2.SimpleBlobDetector_create()
-        keypoints = detector.detect(img)
-        # print(keypoints)
-        blank = np.zeros((1,1))
-        blobs = cv2.drawKeypoints(img, keypoints, blank, (0,255,255), cv2.DRAW_MATCHES_FLAGS_DEFAULT)
+    detector = cv2.SimpleBlobDetector_create(params)
+    keypoints = detector.detect(processed)
+    print(type(keypoints))
+    
+    if debug:
+        blobs = cv2.drawKeypoints(cv_image, keypoints, np.array([]), (0,255,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         cv2.imshow("blobs", blobs)
-        return len(keypoints) > 0
-        # Check if stop signs were detected
-        # if len(stop_signs) > 0:
-        #     return True
-        # else:
-        #     return False
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+
+    pub.publish(keypoints)
+
+
+def stop_sign_detector(self, data):
+    self.bridge = CvBridge()
+    pub = rospy.Publisher('stop_sign', Image, queue_size=10)
+    rospy.init_node('stop_sign_detector', anonymous=True)
+    image_sub = rospy.Subscriber("image_raw", Image, detect)
+    rospy.spin()
+
 
 def main():
-    rospy.init_node('stop_sign_detector', anonymous=True)
-    detector = StopSignDetector()
     try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down")
+        stop_sign_detector()
+    except rospy.ROSInterruptException:
+        pass
 
 if __name__ == '__main__':
     main()
