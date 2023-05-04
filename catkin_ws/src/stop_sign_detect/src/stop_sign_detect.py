76c85bd920cd8748813f1ea2c41c0db32109c4f3
Ben Spicer <bencspicer@gmail.com>
2023-05-04 00:48:27 -0600

    detection tested on real data

--- catkin_ws/src/stop_sign_detect/src/stop_sign_detect.py
+++ catkin_ws/src/stop_sign_detect/src/stop_sign_detect.py
@@ -10,36 +10,53 @@ from cv_bridge import CvBridge, CvBridgeError
 img_array = []
 size = None
 pub = rospy.Publisher('stop_sign', Bool, queue_size=10)
-pub_img = rospy.Publisher('stop_sign_image', Image, queue_size=10)
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+video = cv2.VideoWriter('video.avi', fourcc, 30, (640,480))
  
 
 
 def detect(img):
-    debug = False
-    mode = "dog"
+    debug = True
+    mode = "none"
     
     bridge = CvBridge()
 
     try:
-        cv_image = bridge.imgmsg_to_cv2(img, "rgb8")
+        cv_image = bridge.imgmsg_to_cv2(img, "bgr8")
     except CvBridgeError as e:
         print(e)
 
     # mask only red pixels
-    red_lower = np.array([50, 0, 0]) # BGR
-    red_upper = np.array([255, 150, 150]) # BGR
+    red_lower = np.array([0, 0, 160]) # BGR
+    red_upper = np.array([140, 140, 255]) # BGR
     mask = cv2.inRange(cv_image, red_lower, red_upper)
     detected_output = cv2.bitwise_and(cv_image, cv_image, mask=mask)
 
+    processed = mask
+    # kernel = np.ones((5, 5), np.uint8)
+    kernel = np.array([
+        [0, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [0, 1, 1, 1, 0]
+        ], dtype=np.uint8)
+    # 
+    processed = cv2.erode(processed, kernel, iterations=1)
+    processed = cv2.dilate(processed, kernel, iterations=3)
+    processed = cv2.erode(processed, kernel, iterations=5)
+    processed = cv2.dilate(processed, kernel, iterations=7)
+    processed = cv2.erode(processed, kernel, iterations=9)
+    processed = cv2.dilate(processed, kernel, iterations=11)
+
     # difference of gaussians
     if mode == "lap":
-        blobs_log = cv2.Laplacian(detected_output, cv2.CV_64F)
+        blobs_log = cv2.Laplacian(processed, cv2.CV_64F)
         processed = np.uint8(np.absolute(blobs_log))
     elif mode == "dog":
-        processed = cv2.GaussianBlur(detected_output, (5,5), 0) - cv2.GaussianBlur(detected_output, (15,15), 0)
+        processed = cv2.GaussianBlur(processed, (5,5), 0) - cv2.GaussianBlur(processed, (15,15), 0)
     
-    kernel = np.ones((5, 5), np.uint8)
-    processed = cv2.dilate(processed, kernel, iterations=0)
+    
 
     # if debug:
     #     cv2.imshow("processed", processed)
@@ -49,8 +66,8 @@ def detect(img):
     # Set up the SimpleBlobDetector with default parameters
     params = cv2.SimpleBlobDetector_Params()
 
-    # params.filterByColor = True
-    # params.blobColor = 0
+    params.filterByColor = True
+    params.blobColor = 255
 
     # Set the threshold
     params.minThreshold = 0
@@ -58,17 +75,15 @@ def detect(img):
 
     # # Set the area filter
     params.filterByArea = True
-    scale = 0.3
-    height = processed.shape[0]
-    width = processed.shape[1]
-    size = height * width
-    params.minArea = size*scale
-    # print(size*scale)
-    params.maxArea = np.inf
+    min_scale = 0.015
+    max_scale = 1
+    size = processed.shape[0] * processed.shape[1]
+    params.minArea = size*min_scale
+    params.maxArea = size*max_scale
 
     # Set the circularity filter
-    params.filterByCircularity = False
-    params.minCircularity = .3
+    params.filterByCircularity = True
+    params.minCircularity = .4
     params.maxCircularity = 1
 
     # Set the convexity filter
@@ -78,7 +93,7 @@ def detect(img):
 
     # Set the inertia filter
     params.filterByInertia = False
-    params.minInertiaRatio = 0.1
+    params.minInertiaRatio = 0.15
     params.maxInertiaRatio = 1
 
     detector = cv2.SimpleBlobDetector_create(params)
@@ -88,11 +103,14 @@ def detect(img):
         blobs = cv2.drawKeypoints(cv_image, keypoints, np.array([]), (0,255,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         # height, width, layers = blobs.shape
         # size = (width,height)
+        # print(size)
         # img_array.append(blobs)
+        video.write(blobs)
 
         cv2.imshow("blobs", blobs)
+        cv2.imshow("processed", processed)
         cv2.waitKey(1)
-        cv2.destroyAllWindows()
+        # cv2.destroyAllWindows()
 
     # if debug:
     #     img = bridge.cv2_to_imgmsg(blobs, "rgb8")
@@ -106,15 +124,12 @@ def stop_sign_detector():
     sub = rospy.Subscriber("camera/color/image_raw", Image, detect)
     while not rospy.is_shutdown():
         rospy.spin()
+    video.release()
 
 
 def main():
     try:
         stop_sign_detector()
-        out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
-        for i in range(len(img_array)):
-            out.write(img_array[i])
-        out.release()
     except rospy.ROSInterruptException:
         pass
 
