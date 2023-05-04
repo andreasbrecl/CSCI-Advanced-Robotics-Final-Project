e9d595b1ec034673e308fefdc2c8e6e376e237f5
Daniel Mathews <danielmjr@me.com>
2023-05-03 19:59:39 -0600

    stopping

--- catkin_ws/src/depth_motion/src/depth_listen.py
+++ catkin_ws/src/depth_motion/src/depth_listen.py
@@ -13,11 +13,14 @@ Date: 04/25/2023
 import rospy
 import cv2
 from sensor_msgs.msg import Image as msg_Image
+from sensor_msgs.msg import Imu
 from sensor_msgs.msg import CameraInfo
-from std_msgs.msg import String
+from std_msgs.msg import String, Bool
 from cv_bridge import CvBridge, CvBridgeError
+from tf.transformations import euler_from_quaternion
 import numpy as np
 import time
+import math
 
 
 class ImageListener:
@@ -30,30 +33,83 @@ class ImageListener:
         self.bridge = CvBridge()
 
         # Subscribe to camera topic
-        self.sub = rospy.Subscriber(depth_image_topic, msg_Image, self.imageDepthCallback)
+        self.imu_sub = rospy.Subscriber('/imu/data', Imu, self.imu_callback, queue_size=1)
+        self.sub = rospy.Subscriber(depth_image_topic, msg_Image, self.image_depth_callback)
+        self.sub = rospy.Subscriber('stop', Bool, self.stop_callback)
 
         # Create published topics
         self.pub_cmd = rospy.Publisher('control_cmd', String, queue_size=1)
         self.pub_w = rospy.Publisher('contour_w', String, queue_size=1)
         self.pub_plot = rospy.Publisher('contourPlot', msg_Image, queue_size=1)
+        self.diff_angle = rospy.Publisher('control_diff', String, queue_size=1)
 
         # Define iterators for logic
         self.count = 0
         self.turn_counter = 0
-        self.stop_counter = 0
         self.turn_timer = time.time()
+        self.in_turn_bool = False
+        self.in_straight_bool = False
+        self.hit_obj_bool = False
+        self.hit_rev_bool = False
+        self.in_wait_bool = False
+        self.stop_timer = 10
 
-    def imageDepthCallback(self, data):
+        # Define IMU variables
+        self.imu_yaw_check = 0
+        self.imu_yaw_current = 0
+        self.imu_yaw_no_block = 0
+        self.imu_yaw_block = 0
+        self.imu_yaw_block_current = 0
+        self.turn_angle = 65
+
+    def imu_callback(self, imu_data):
         """
-        This function determines the logic for location and motion.
-
-        Inputs:  data <array> - Depth image information from D450
+        Initialize the Imu.
+        
+        Inputs:  imu_data <array> - Imu data
 
         Outputs: None
         """
+        self.imu_data_current = imu_data
+        self.calculate_motion()
+
+    def stop_callback(self, stop_bool):
+        """
+        Initialize the Imu.
+        
+        Inputs:  imu_data <array> - Imu data
+
+        Outputs: None
+        """
+        self.stop_bool = stop_bool
+        self.calculate_motion()
+
+    def image_depth_callback(self, data):
+        """
+        Initialize the image.
+        
+        Inputs:  data <array> - Image data
+
+        Outputs: None
+        """
+        self.camera_data_current = data
+        self.calculate_motion()
+
+    def calculate_motion(self):
+        """
+        This function determines the logic for location and motion.
+
+        Inputs:  None
+
+        Outputs: None
+        """
+        # Check if data is present
+        if self.imu_data_current is None or self.camera_data_current is None:
+            return
+
         try:
             # Convert data image via open cv
-            cv_image = self.bridge.imgmsg_to_cv2(data, data.encoding)
+            cv_image = self.bridge.imgmsg_to_cv2(self.camera_data_current, self.camera_data_current.encoding)
 
             # Crop the immage
             #crop_image = cv_image[240:480,0:848]
@@ -81,40 +137,223 @@ class ImageListener:
                 cmdAng = round(-17+(30*int(center_pt)/848)) # degrees min: -25, max: 25
                 cmdVel = 3 # velocity min: 0, max: 9
 
+                # Check if wall is not being hit
+                print(self.imu_data_current.linear_acceleration.y)
+                if self.imu_data_current.linear_acceleration.y < 4 and self.hit_obj_bool != True and self.hit_rev_bool != True and self.in_wait_bool != True:
+                        
+                    # Pull current IMU data
+                    quaternion = (
+                        self.imu_data_current.orientation.x,
+                        self.imu_data_current.orientation.y,
+                        self.imu_data_current.orientation.z,
+                        self.imu_data_current.orientation.w
+                    )
+                    _, _, yaw = euler_from_quaternion(quaternion)
+                    yaw = math.degrees(yaw)
+                    self.imu_yaw_no_block = yaw
+
+                # Trigger if wall is hit
+                elif self.hit_obj_bool != True and self.hit_rev_bool != True and self.in_wait_bool != True:
+                    cmdVel = -3
+                    cmdAng = 0
+                    self.sendCommand(cmdAng, cmdVel, contImage, w, 'NA')  
+                    self.in_turn_bool = False
+                    self.in_straight_bool = False
+                    self.hit_obj_bool = True
+                    self.turn_timer = time.time()
+
+                if self.stop_bool == True and self.stop_timer > 5:
+                    timer = time.time()
+                    while time.time() - timer < 5:
+                        cmdAng = 0
+                        cmdVel = 0
+                        self.sendCommand(cmdAng, cmdVel, contImage, w, 'NA')
+                    self.stop_timer = time.time()
+
+                # Start hit object motion
+                if self.hit_obj_bool == True:
+
+                    # Check if time has passed
+                    if (time.time() - self.turn_timer) > 2:
+                        cmdVel = 0
+                        cmdAng = 0
+                        self.sendCommand(cmdAng, cmdVel, contImage, w, 'NA')
+
+                        # Enter turn reverse mode
+                        self.hit_rev_bool = True
+                        self.hit_obj_bool = False      
+                    
+                    else:
+                        # Stop car
+                        cmdVel = -3
+                        cmdAng = 0
+                        self.sendCommand(cmdAng, cmdVel, contImage, w, 'NA')
+
+                # Enter turn condition
+                elif self.hit_rev_bool == True:
+
+                    # Set speed values
+                    cmdVel = -3
+
+                    # Check current angle
+                    quaternion = (
+                        self.imu_data_current.orientation.x,
+                        self.imu_data_current.orientation.y,
+                        self.imu_data_current.orientation.z,
+                        self.imu_data_current.orientation.w
+                    )
+                    _, _, yaw = euler_from_quaternion(quaternion)
+                    yaw = math.degrees(yaw)
+                    self.imu_yaw_block_current = yaw
+
+                    # Calculate angle difference of system
+                    turn_block_angle = self.imu_yaw_no_block - self.imu_yaw_block_current
+
+                    # Check if can has fully turned
+                    if abs(turn_block_angle) > 5:
+
+                        # Send straight command and reset boolean
+                        cmdAng = turn_block_angle
+                        self.sendCommand(cmdAng, cmdVel, contImage, w, 'NA')
+
+                    else:
+                        
+                        # Send turn command
+                        self.hit_rev_bool = False
+                        self.in_wait_bool = True
+                        cmdAng = 0
+                        self.sendCommand(cmdAng, cmdVel, contImage, w, 'NA')
+
+                # Wait for object removal
+                elif self.in_wait_bool == True:
+
+                    # Check if time has passed
+                    if (time.time() - self.turn_timer) > 5:
+                        cmdVel = 0
+                        cmdAng = 0
+                        self.sendCommand(cmdAng, cmdVel, contImage, w, 'NA')
+
+                        # Enter normal mode
+                        self.in_wait_bool = False    
+                    
+                    else:
+                        # Stop car
+                        cmdVel = 0
+                        cmdAng = 0
+                        self.sendCommand(cmdAng, cmdVel, contImage, w, 'NA')
+
+                # Handle straight condition
+                elif self.in_straight_bool == True:
+
+                    # Set speed values
+                    cmdVel = 3
+
+                    # Check if time has passed
+                    if (time.time() - self.turn_timer) < .6:
+                        
+                        # Send command
+                        print("Turn")
+                        cmdAng = 0
+                        self.sendCommand(cmdAng, cmdVel, contImage, w, 'NA')
+
+                    else:
+
+                        # Reset boolean values
+                        self.in_straight_bool = False
+                        self.in_turn_bool = True
+
+                        # Pull current IMU data
+                        quaternion = (
+                            self.imu_data_current.orientation.x,
+                            self.imu_data_current.orientation.y,
+                            self.imu_data_current.orientation.z,
+                            self.imu_data_current.orientation.w
+                        )
+                        _, _, yaw = euler_from_quaternion(quaternion)
+                        yaw = math.degrees(yaw)
+                        self.imu_yaw_check = yaw
+
+                        # Send turn command
+                        cmdAng = 15
+                        self.sendCommand(cmdAng, cmdVel, contImage, w, 'NA')
+
+                # Handle turn condition
+                elif self.in_turn_bool == True:
+
+                    # Set speed values
+                    cmdVel = 3
+
+                    # Check current angle
+                    quaternion = (
+                        self.imu_data_current.orientation.x,
+                        self.imu_data_current.orientation.y,
+                        self.imu_data_current.orientation.z,
+                        self.imu_data_current.orientation.w
+                    )
+                    _, _, yaw = euler_from_quaternion(quaternion)
+                    yaw = math.degrees(yaw)
+                    self.imu_yaw_current = yaw
+
+                    # Check if can has fully turned
+                    diff = abs((self.imu_yaw_current - self.imu_yaw_check + 180) % 360 - 180)
+                    if diff > self.turn_angle:
+
+                        # Send straight command and reset boolean
+                        self.in_turn_bool = False
+                        cmdAng = 0
+                        self.sendCommand(cmdAng, cmdVel, contImage, w, str(diff))
+
+                    else:
+                        
+                        # Send turn command
+                        cmdAng = 15
+                        self.sendCommand(cmdAng, cmdVel, contImage, w, str(diff))
+
                 # Check if vehicle is approaching wall
-                if w < 100:
+                elif w < 100:
+
+                    # Set speed values
+                    cmdVel = 3
+
                     # Iterate counter
                     self.count += 1
 
-                # Check if close value is not random
-                if self.count > 2:
-                    
-                    # Increment turn counter
-                    self.turn_counter += 1
-
-                    # Count 4th wall
-                    if np.mod(self.turn_counter,4) != 0:
-                        cmdAng = 0
-                        self.sendCommand(cmdAng, cmdVel, contImage, w)
-
-                    if self.stop_counter < 4:
-                        # Command turn
-                        cmdAng = 15
-                        self.sendCommand(cmdAng, cmdVel, contImage, w)
-
-                        # Increment Counter
-                        if w > 100:
-                            self.stop_counter += 1
-                    else:
-                        # Reset turn logic variables
-                        cmdAng = 0
-                        self.sendCommand(cmdAng, cmdVel, contImage, w)
+                    # Check if wall conditions is met
+                    if self.count > 2:
+                            
+                        # Set straight to true
                         self.turn_timer = time.time()
+                        self.in_straight_bool = True
+
+                        # Send commmand
+                        cmdAng = 0
+                        self.sendCommand(cmdAng, cmdVel, contImage, w, 'NA')
+
+                        # Reset counter
                         self.count = 0
-                        self.stop_counter = 0
-                else:
+
                     # If not near wall operate normal operation
-                    self.sendCommand(cmdAng, cmdVel, contImage, w)
+                    else:
+
+                        # Send movement command
+                        self.sendCommand(cmdAng, cmdVel, contImage, w, 'NA')
+
+                # Slow down before walls
+                #elif w > 100 and w < 140:
+                    # Set speed values
+                #    cmdVel = 3
+
+                    # Send movement command
+                #    self.sendCommand(cmdAng, cmdVel, contImage, w, 'NA')
+
+
+                # Normal operations
+                else:
+                    # Set speed values
+                    cmdVel = 3
+
+                    # Send movement command
+                    self.sendCommand(cmdAng, cmdVel, contImage, w, 'NA')
 
         except CvBridgeError as e:
             print(e)
@@ -122,7 +361,7 @@ class ImageListener:
         except ValueError as e:
             return
 
-    def sendCommand(self, cmdAng, cmdVel, contImage, w):
+    def sendCommand(self, cmdAng, cmdVel, contImage, w, diff):
         """
         This function handels sending commands for angle and velocity
 
@@ -140,6 +379,7 @@ class ImageListener:
         self.pub_cmd.publish(control_str)
         self.pub_w.publish(str(w))
         self.pub_plot.publish(contImage)
+        self.diff_angle.publish(diff)
 
 def main():
     """
